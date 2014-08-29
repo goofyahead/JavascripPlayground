309588fe544b68fe5107294b29cc80440f07764a
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-08-29 15:51:00 +0200

    test reboot

--- test-watch-folder/app.js
+++ test-watch-folder/app.js
@@ -9,45 +9,17 @@ fs.watch('files', function (event, filename) {
   }
 });
 
+var proc = require('child-process').spawn;
+
 var ping = require('net-ping');
 var session = ping.createSession ();
 
 session.pingHost ('192.168.1.99', function (error, target) {
-    if (error)
-        console.log (target + ": " + error.toString ());
+    if (error) {
+    	var reboot = proc('reboot');
+    	console.log('No internet, changing files and rebooting on HOTSPOT'.green);
+    }
     else
         console.log (target + ": Alive");
 });
-// Require
-// var watchr = require('watchr');
 
-// // Watch a directory or file
-// console.log('Watch our paths');
-// watchr.watch({
-//     path: 'files',
-//     listeners: {
-//         log: function(logLevel){
-//             console.log('a log message occured:', arguments);
-//         },
-//         error: function(err){
-//             console.log('an error occured:', err);
-//         },
-//         watching: function(err,watcherInstance,isWatching){
-//             if (err) {
-//                 console.log("watching the path " + watcherInstance.path + " failed with error", err);
-//             } else {
-//                 console.log("watching the path " + watcherInstance.path + " completed");
-//             }
-//         },
-//         change: function(changeType,filePath,fileCurrentStat,filePreviousStat){
-//             console.log('a change event occured:',arguments);
-//         }
-//     },
-//     next: function(err,watchers){
-//         if (err) {
-//             return console.log("watching everything failed with error", err);
-//         } else {
-//             console.log('watching everything completed', watchers);
-//         }
-//     }
-// });
