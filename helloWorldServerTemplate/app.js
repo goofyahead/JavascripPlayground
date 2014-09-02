eedd4014846ef30b294462ccae01cee4e5c9ca19
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-09-02 13:12:47 +0200

    reboot from web

--- helloWorldServerTemplate/app.js
+++ helloWorldServerTemplate/app.js
@@ -3,6 +3,21 @@ var app = express();
 
 app.get('/', function(req, res){
   res.send('hello world');
-});
+
+  var spawn = require('child_process').spawn,
+    ls    = spawn('reboot');
+
+	ls.stdout.on('data', function (data) {
+	  console.log('stdout: ' + data);
+	});
+
+	ls.stderr.on('data', function (data) {
+	  console.log('stderr: ' + data);
+	});
+
+	ls.on('close', function (code) {
+	  console.log('child process exited with code ' + code);
+	});
+	});
 
 app.listen(3000);
