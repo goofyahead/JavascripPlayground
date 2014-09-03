51026b13eafd7c7cb3d29b9afef26ac692168e39
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-09-03 17:22:38 +0200

    config web

--- helloWorldServerTemplate/app.js
+++ helloWorldServerTemplate/app.js
@@ -1,23 +1,62 @@
 var express = require('express');
 var app = express();
+var ping = require('net-ping');
+var session = ping.createSession ();
+var config = require('./config')
+var redis = require("redis"),
+client = redis.createClient();
+var colors = require ('colors');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var favicon = require('static-favicon');
 
-app.get('/', function(req, res){
-  res.send('hello world');
+var HOTSPOTMODE = "HOTSPOTMODE";
 
-  var spawn = require('child_process').spawn,
-    ls    = spawn('reboot');
+setTimeout(function () {
+	//check if its on hotstpot mode
+	client.get(HOTSPOTMODE, function (err, reply) {
+		if (reply && reply == 1) {
+			console.log('STARTING SERVER FOR CONFIG NETWORK');
+			// in hotspot mode wait for config and reboot on click
+			app.set('views', __dirname + '/views');
+			app.use(favicon());
+			app.use(bodyParser.json());
+			app.use(bodyParser.urlencoded());
+			app.use(cookieParser());
+			app.engine('.html', require('ejs').renderFile);
+			app.set('view engine', 'html');
 
-	ls.stdout.on('data', function (data) {
-	  console.log('stdout: ' + data);
+			app.post('/config', function (req, res) {
+				var ssid = req.param('ssid');
+				var password = req.param('pass');
+
+				console.log('configuration received: '.yellow + password + ' ssid ' + ssid);
+				config.toConnected(ssid, password);
+			});
+
+			app.get('/', function (req, res){
+				res.render('form');
+			});
+
+			app.listen(3000);
+
+		} else {
+			console.log('CHECKING CONNECTION');
+			// if not in hotspot, check if internet, if not internet go to hotspot and reboot
+			session.pingHost ('8.8.8.8', function (error, target) {
+				if (error) {
+					console.log('No internet, changing files and rebooting on HOTSPOT'.green);
+					config.toHostspot();
+				}
+				else {
+					console.log('RASPBERRY PI ONLINE'.green);
+					console.log (target + ": Alive");
+				}
+			});
+		}
 	});
 
-	ls.stderr.on('data', function (data) {
-	  console.log('stderr: ' + data);
-	});
+}, 1000);
 
-	ls.on('close', function (code) {
-	  console.log('child process exited with code ' + code);
-	});
-	});
 
-app.listen(3000);
+
---
+++ helloWorldServerTemplate/config.js
@@ -0,0 +1,109 @@
+var fs = require('fs');
+var async = require('async');
+var colors = require('colors');
+
+exports.toHotspot = function () {
+	async.parallel([
+		function(callback){
+			fs.readFile('./files/daemonOn.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing daemon file /etc/default/udhcpd');
+				fs.writeFile('/etc/default/udhcpd', data, function(err) {
+					if (err) throw err;
+					console.log('DAEMON configured'.green);
+				});
+			});
+		}, function (callback) {
+			fs.readFile('./files/ifaceHotspot.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing interfaces file /etc/network/interfaces');
+				fs.writeFile('/etc/network/inferfaces', data, function (err){
+					if (err) throw err;
+					console.log('interface configured for Hotspot'.green);
+				});
+			});
+		}, function (callback) {
+			fs.readFile('./enabledUDHCPD.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing UDHCP file /etc/default/udhcpd');
+				fs.writeFile('/etc/default/udhcpd', data, function (err) {
+					if (err) throw err;
+					console.log('UDHCPD enabled'.green);
+				});
+			});
+	}],// optional callback
+	function(err, results){
+		if (err) throw err;
+		console.log('config files updated, rebooting...');
+		reboot();
+	});
+}
+
+exports.toConnected = function (SSID, password) {
+
+	async.parallel([
+		function(callback){
+			fs.readFile('./files/daemonOFF.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing daemon file /etc/default/udhcpd');
+				fs.writeFile('/etc/default/udhcpd', data, function(err) {
+					if (err) throw err;
+					console.log('DAEMON configured'.green);
+					callback(null);
+				});
+			});
+		}, function (callback){
+			fs.readFile('./files/ifaceWifi.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing interfaces file /etc/network/interfaces');
+				fs.writeFile('/etc/network/inferfaces', data, function (err){
+					if (err) throw err;
+					console.log('interface configured for Hotspot'.green);
+					fs.appendFileSync('/etc/network/interfaces', '\twpa-ssid "' + SSID + '"\n', function (err) {
+						if (err) throw err;
+						console.log('The "data to append" was appended to file!');
+					});
+					fs.appendFileSync('/etc/network/interfaces', '\twpa-psk "' + password + '"\n', function (err) {
+						if (err) throw err;
+						console.log('The "data to append" was appended to file!');
+					});
+
+					callback(null);
+				});
+			});   
+		}, function (callback){
+			fs.readFile('./disabledUDHCPD.txt', function (err, data) {
+				if (err) throw err;
+				console.log('writing UDHCP file /etc/default/udhcpd');
+				fs.writeFile('/etc/default/udhcpd', data, function (err) {
+					if (err) throw err;
+					console.log('UDHCPD enabled'.green);
+					callback(null);
+				});
+			});
+		}],
+	// optional callback
+	function(err, results){
+		if (err) throw err;
+		console.log('config files updated, rebooting...');
+		reboot();
+	});
+}
+
+function reboot() {
+	var spawn = require('child_process').spawn,
+				ls    = spawn('reboot');
+
+				ls.stdout.on('data', function (data) {
+					console.log('stdout: ' + data);
+				});
+
+				ls.stderr.on('data', function (data) {
+					console.log('stderr: ' + data);
+				});
+
+				ls.on('close', function (code) {
+					console.log('child process exited with code ' + code);
+				});
+}
+
