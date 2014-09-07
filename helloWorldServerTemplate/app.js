ef8c146bc5f77bc5686d239995d8770f7948cf2c
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-09-07 23:21:15 +0200

    updates

--- helloWorldServerTemplate/app.js
+++ helloWorldServerTemplate/app.js
@@ -11,8 +11,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var favicon = require('static-favicon');
 
-
-
 setTimeout(function () {
 	//check if its on hotstpot mode
 	client.get(HOTSPOTMODE, function (err, reply) {
@@ -36,23 +34,56 @@ setTimeout(function () {
 			});
 		}
 	});
-}, 5000);
+}, 1000);
 
 function setUpServer() {
 	app.set('views', __dirname + '/views');
 	app.use(favicon());
+
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded());
 	app.use(cookieParser());
 	app.engine('.html', require('ejs').renderFile);
 	app.set('view engine', 'html');
+	app.use(express.static(__dirname + '/public'));
+
+	var multipart = require('connect-multiparty');
+	var multipartMiddleware = multipart();
+
+	app.post('/upload', multipartMiddleware, function (req, res) {
+		console.log('call received');
+		var video = req.files;
+		console.log(video);
+		res.status(200).end();
+		//delete all temp files!
+	});
 
 	app.post('/config', function (req, res) {
+		var crypto = require('crypto');
+
 		var ssid = req.param('ssid');
 		var password = req.param('pass');
+		var host = req.param('host');
+		var username = req.param('username');
+		var userpass = req.param('userpass');
+		var shashum = crypto.createHash('sha256');
+		var content = username + ":" + userpass;
+		
+		var cyphered = shashum.update(content).digest('hex');
 
-		console.log('configuration received: '.yellow + password + ' ssid ' + ssid);
-		config.toConnected(ssid, password);
+		console.log(cyphered);
+
+		client.set('HOSTADDRESS', host);
+		client.set('HASH', cyphered);
+
+		if (ssid) {	
+			console.log('configuration received: '.yellow + password + ' ssid ' + ssid);
+			config.toConnected(ssid, password);
+		} else {
+			console.log('just saving host and client.'.yellow);
+		}
+
+		res.render('saving');
 	});
 
 	app.get('/', function (req, res){
